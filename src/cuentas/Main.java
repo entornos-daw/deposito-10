eed9f974dc0c5b1ca50567288ddf2385d6ad9b10
Javier <Javier@DESKTOP-B8EL5SK>
2021-03-09 12:26:17 +0100

    Modificación saldo Antonio López

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -20,7 +20,7 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
         
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",10500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es "+ saldoActual);
         
